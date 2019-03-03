450531ee1af93d82831b0792c1e7771563265a00
pablo Coronel <pcoronel@untref.edu.ar>
2019-03-02 21:22:47 -0300

    delete folder

--- Version archivos ONLINE/conexion_DB/conexion_DB.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host='localhost';
-$user='pablocoronel_pc';
-$pass='9Argentina9';
-$db='pablocoronel_webvideos';
-
-$conexion= mysqli_connect($host, $user, $pass, $db) or die('error al conectar');
-?>
